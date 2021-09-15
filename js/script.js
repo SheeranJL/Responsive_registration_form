535773e0df760b9a24f3f86b5e4c38dfb60812eb
SheeranJL <lewisjsheeran@protonmail.com>
2021-09-15 15:39:19 +1000

    Edits and changes

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ const jobRoleSelection = document.querySelector('#title');
 const colorSelection = document.querySelector('#color');
 colorSelection.disabled = 'true';
 designSelection = document.querySelector('#design')
-
+form = document.querySelector('form');
+const singleActivity = document.querySelector('[value = "full-stack js developer"]');
+const jobSelection = document.getElementById('jobs');
 
 // this code will listen to the 'other' selection being targeted and if so, it will show the 'other' field.
 jobRoleSelection.addEventListener('change', (e) => {
@@ -51,6 +53,49 @@ let activityFieldset = document.getElementById('activities'); //This selects the
 let labelCost = document.getElementById('activities-cost'); //This is for the total cost shown on form
 let totalAmount = 0; //total amount for product selection
 
+let paymentMethod = document.querySelector('#payment');
+
+
+function showHideMenu(id, toggle) {
+  document.querySelector(id).hidden = toggle;
+}
+
+function hideShowFlex(id, toggle) {
+  document.querySelector(id).style.display = toggle;
+}
+
+///Select Credit Card as default opton on page load///
+function defaultPaymentOption() {
+  const defaultOption = document.querySelector('[value = "credit-card"]');
+  defaultOption.selected = true;
+}
+defaultPaymentOption();
+
+
+/// Hide/Show relevant payment options and information on selection ///
+function paymentMethodInfo() {
+  const cardInfo = document.getElementById('credit-card');
+  const paypalInfo = document.getElementById('paypal');
+  const bitcoinInfo = document.getElementById('bitcoin');
+  paypalInfo.style.display = 'none';
+  bitcoinInfo.style.display = 'none';
+
+  paymentMethod.addEventListener('change', (event) => {
+    cardInfo.style.display = 'none';
+    paypalInfo.style.display = 'none';
+    bitcoinInfo.style.display = 'none';
+    if (event.target.value === 'credit-card') {
+      cardInfo.style.display = '';
+    } else if (event.target.value === 'paypal') {
+      paypalInfo.style.display = '';
+    } else {
+      bitcoinInfo.style.display = '';
+    }
+  })
+};
+paymentMethodInfo();
+
+
 //event listener on the fieldset to listen to any change events within checkbox elements
 activityFieldset.addEventListener('change', (e) => {
   let target = event.target; //grabbing the event element
@@ -64,90 +109,105 @@ activityFieldset.addEventListener('change', (e) => {
 });
 
 
-////****Payment Section****////
+//Field selectors///
 
-let payment = document.querySelector('.payment-methods');
-
-function showHideMenu(id, toggle) {
-  document.querySelector(id).hidden = toggle;
-}
-
-function hideShowFlex(id, toggle) {
-  document.querySelector(id).style.display = toggle;
-}
-
-payment.addEventListener('change', (e) => {
-  let selection = e.target;
-  if (selection.value === 'credit-card') {
-    showHideMenu('#paypal', 'true');
-    showHideMenu('#bitcoin', 'true');
-    hideShowFlex('#credit-card', '');
-  } else if (selection.value === 'paypal') {
-    showHideMenu('#paypal', '');
-    showHideMenu('#bitcoin', 'true');
-    hideShowFlex('#credit-card', 'none');
-  } else if (selection.value === 'bitcoin') {
-    showHideMenu('#paypal', 'true')
-    showHideMenu('#bitcoin', '');
-    hideShowFlex('#credit-card', 'none');
-  }
-});
-
+const nameField = document.getElementById('name');
+const emailField = document.getElementById('email');
+const cardNumberField = document.getElementById('cc-num')
+const zipCodeField = document.getElementById('zip');
+const cvvField = document.getElementById('cvv');
+const activitySelect = document.getElementById('activities')
 ///***Form Validation Section***///
 
-let submit = document.querySelector('button[type="submit"]');
-let form = document.querySelector('form');
-let nameField = document.querySelector('#name');
-let emailField = document.querySelector('#email');
-let jobTitle = document.querySelector('#title');
-let creditCardInfo = document.querySelector('#credit-card');
-let zipCode = document.querySelector('#zip');
-let cvvCode = document.querySelector('#cvv')
-let creditCardNum = document.querySelector('#cc-num');
-
-form.addEventListener('submit', (e) => {
-  let target = event.target;
+function validateName() {
   let nameReg = /\S+/;
-  let emailReg = /^[^@]+@[^@.]+\.[a-z]+$/i;
-  let cardNumReg = /^(\d{13,16})$/g;
-  let zipCodeReg = /^(\d{5})$/g;
-  let cvvReg = /^(\d{3})$/g;
+  return nameReg.test(nameField.value);
+}
 
-  if (nameReg.test(nameField.value) === false) {
-    alert('You must enter a name');
-    event.preventDefault()
+function validateEmail() {
+  let emailReg = /^[^@]+@[^@.]+\.[a-z]+$/i;
+  return emailReg.test(emailField.value);
+}
+
+function jobSelect() {
+  if (jobRoleSelection.value === 'Select Job Role') {
+    return false
+  } else {
+    return true;
   }
-  else if (emailReg.test(emailField.value) === false) {
-    alert('You must enter a valid email');
-    event.preventDefault()
+}
+
+function validateActivitySelection() {
+  if (totalAmount === 0) {
+    return false;
+  } else {
+    return true;
   }
-  else if (jobTitle.value === 'Select Job Role') {
-    alert('You must select a job title');
-    event.preventDefault()
+}
+
+function validateCardNum () {
+  const cardNumReg = /^(\d{13,16})$/g;
+  return cardNumReg.test(cardNumberField.value);
+}
+
+function validateZipCode() {
+  const zipCodeReg = /^(\d{5})$/g;
+  return zipCodeReg.test(zipCodeField.value);
+}
+
+function validateCVV() {
+  const cvvReg = /^(\d{3})$/g;
+  return cvvReg.test(cvvField.value);
+}
+
+function validateAllFunction(functionName, element, event, message) {
+  if (functionName() === false) {
+    element.parentElement.classList.add('not-valid');
+    element.parentElement.classList.remove('valid');
+    element.parentElement.lastElementChild.classList.remove('hint');
+    console.log(message)
+    event.preventDefault();
+  } else {
+    element.parentElement.classList.add('valid');
+    element.parentElement.classList.remove('not-valid');
+    element.parentElement.lastElementChild.classList.add('hint');
   }
-  else if (creditCardInfo.style.display === '') { //if the credit card info is showing....
-    // add if statements here
-    if (cardNumReg.test(creditCardNum.value) === false) {
-      alert('Enter a card number 13 or 16 numbers long with no spaces or dashes')
-      event.preventDefault()
-    }
-    else if (zipCodeReg.test(zipCode.value) === false) {
-      alert('You must enter a 5 digit zip code');
-      event.preventDefault()
-    }
-    else if (cvvReg.test(cvvCode.value) === false) {
-      alert('You must enter a 3 digit cvv');
-      event.preventDefault()
-    }
-    else {
-      console.log('success');
-      //event.preventDefault()
-    }
-    } else {
-      console.log('success');
+}
+
+function validateJobSelection(functionName, element, event, message) {
+  if (functionName() === false) {
+    element.style.borderColor = 'red';
+    console.log('No Job Selected')
+    event.preventDefault();
+    jobSelection.lastElementChild.classList.remove('hint');
+  } else {
+    element.style.borderColor = '';
+    element.parentElement.classList.add('valid');
+    jobSelection.lastElementChild.classList.add('hint');
+  }
+}
+
+form.addEventListener('submit', (event) => {
+  const target = event.target;
+  const creditCardSelection = document.getElementById('credit-card');
+
+  validateAllFunction(validateName, nameField, event, 'invalid name');
+  validateAllFunction(validateEmail, emailField, event, 'invalid email');
+  validateAllFunction(validateActivitySelection, activitySelect.firstElementChild, event, 'No Activity Selected')
+  validateJobSelection(jobSelect, jobRoleSelection, event, 'No Job Role Selected')
+
+  if (creditCardSelection.style.display === '') {
+    validateAllFunction(validateCardNum, cardNumberField, event, 'invalid card number')
+    validateAllFunction(validateZipCode, zipCodeField, event, 'invalid ZIP code')
+    validateAllFunction(validateCVV, cvvField, event, 'invalid CVV')
   }
 });
 
+///***Form event Handler***///
+form.addEventListener('submit', (e) => {
+let target = event.target;
+
+});
 
 ///***Accessibility***///
 
