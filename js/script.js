d63a495135dd329a2dd49c4e4742bcff2cd9500a
SheeranJL <lewisjsheeran@protonmail.com>
2021-09-19 12:19:59 +1000

    Reupload of project 7
    Changed most of the code to do things differently. Also made the fields validate in real-time as well as on form submission.

--- js/script.js
+++ js/script.js
@@ -1,232 +1,283 @@
-///***VARIABLES***///
-const otherText = document.querySelector('#other-job-role');
-  otherText.hidden = true;
-const jobRoleSelection = document.querySelector('#title');
-const colorSelection = document.querySelector('#color');
-  colorSelection.disabled = 'true';
-const designSelection = document.querySelector('#design')
-const form = document.querySelector('form');
-const singleActivity = document.querySelector('[value = "full-stack js developer"]');
-const jobSelection = document.getElementById('jobs');
-const jsPunsShirt = document.querySelectorAll('[data-theme = "js puns"]');
-const jsHeartShirt = document.querySelectorAll('[data-theme = "heart js"]');
+let title = document.getElementById('title');
+let otherField = document.getElementById('other-job-role')
+let nameField = document.getElementById('name');
+let emailField = document.getElementById('email');
+let cardNumField = document.getElementById('cc-num');
+let zipCodeField = document.getElementById('zip');
+let cvvCodeField = document.getElementById('cvv');
+let activitiesBox = document.querySelectorAll('[type="checkbox"]');
+nameField.focus();
 
-document.querySelector('input').focus();
+/// SHOW/HIDE 'OTHER' JOB ROLE ///
 
-///***JOB 'OTHER' SELECTION HANDLER***///
-// this code will listen to the 'other' selection being targeted and if so, it will show the 'other' field.
-jobRoleSelection.addEventListener('change', (e) => {
-  target = e.target.value
+otherField.hidden = true //Hide by default on load
+
+title.addEventListener('change', (e) =>{
+  target = e.target.value;
   if (target === 'other') {
-    otherText.hidden = false;
+    otherField.hidden = false;
   } else {
-    otherText.hidden = true;
+    otherField.hidden = true;
   }
 });
 
-///***SHIRT TOGGLE HELPER FUNCTION***///
-// function to select short color types. This will be used when displaying the correct shirt options
-let shirtToggle = (nodeList, toggle) => {
-  Array.from(nodeList).forEach((item) => {
-    item.hidden = toggle;
-  })
-};
 
-// this code will listen for a change in the selection dropdown and will 'show' the color menu when an option is selected.
-designSelection.addEventListener('change', (e) => {
-  target = e.target.type;
-  shirtType = e.target.value;
-  console.log(target);
-  console.log(shirtType);
-    if (target === 'select-one') {
-      colorSelection.disabled = '';
+//// Shirt style ////
+let designDropdown = document.getElementById('design');
+let designColorOptions = document.getElementById('color').children;
+
+
+
+///helper function ///
+let styleFilter = (theme, toggleOne, toggleTwo) => {
+  Array.from(designColorOptions).forEach((color) => {
+    if (color.dataset.theme === theme) {
+      color.hidden = toggleOne;
     } else {
-      colorSelection.disabled = 'true'
+      color.hidden = toggleTwo;
     }
-
-    //this code will display only the available colors depending on what shirt is selected
-    if (shirtType === 'js puns') {
-      shirtToggle(jsHeartShirt, 'true');
-      shirtToggle(jsPunsShirt, '');
-    }
-    else if (shirtType === 'heart js') {
-      shirtToggle(jsPunsShirt, 'true');
-      shirtToggle(jsHeartShirt, '');
-    }
-});
-
-////****Costing section****////
-let activityFieldset = document.getElementById('activities'); //This selects the fieldset
-let labelCost = document.getElementById('activities-cost'); //This is for the total cost shown on form
-let totalAmount = 0; //total amount for product selection
-let paymentMethod = document.querySelector('#payment');
-
-///Select Credit Card as default opton on page load///
-function defaultPaymentOption() {
-  const defaultOption = document.querySelector('[value = "credit-card"]');
-  defaultOption.selected = true;
+  })
 }
 
-defaultPaymentOption();
-
-
-/// Hide/Show relevant payment options and information on selection ///
-function paymentMethodInfo() {
-  const cardInfo = document.getElementById('credit-card');
-  const paypalInfo = document.getElementById('paypal');
-  const bitcoinInfo = document.getElementById('bitcoin');
-  paypalInfo.style.display = 'none';
-  bitcoinInfo.style.display = 'none';
-
-  paymentMethod.addEventListener('change', (event) => {
-    cardInfo.style.display = 'none';
-    paypalInfo.style.display = 'none';
-    bitcoinInfo.style.display = 'none';
-    if (event.target.value === 'credit-card') {
-      cardInfo.style.display = '';
-    } else if (event.target.value === 'paypal') {
-      paypalInfo.style.display = '';
-    } else {
-      bitcoinInfo.style.display = '';
-    }
-  })
-};
-
-paymentMethodInfo();
-
-
-let activitiesBox = document.querySelector('#activities-box');
-//event listener on the fieldset to listen to any change events within checkbox elements
-activityFieldset.addEventListener('change', (e) => {
-  let target = event.target; //grabbing the event element
-  let ticketPrice = parseInt(target.getAttribute('data-cost')); //grabbing product cost and converting to integer
-  if (target.checked) { //if target is checked
-    totalAmount += ticketPrice; //increase the totalAmount by the ticketPrice
-  } else if (target.checked === false) { //else if the checkbox is not checked (or unchecked)
-    totalAmount -= ticketPrice; //minus the totalAmount by the ticketPrice;
+let colorDropdown = document.getElementById('color');
+colorDropdown.disabled = true;
+//Event Listener on Design Dropdown//
+designDropdown.addEventListener('change', (e) => {
+  target = e.target.value;
+  console.log(target);
+  if (target === 'js puns') {
+    colorDropdown[0].style.display = 'none';
+    colorDropdown.disabled = false;
+    styleFilter('heart js', true, false);
+  } else {
+    colorDropdown[0].style.display = 'none';
+    colorDropdown.disabled = false;
+    styleFilter('js puns', true, false);
   }
-  labelCost.innerHTML = `Total: $${totalAmount}`; //setting the innerHTML for the total price using template literals
 });
 
 
-const nameField = document.getElementById('name');
-const emailField = document.getElementById('email');
-const cardNumberField = document.getElementById('cc-num')
-const zipCodeField = document.getElementById('zip');
-const cvvField = document.getElementById('cvv');
-const activitySelect = document.getElementById('activities')
+///Activities Section///
 
+const activitySectionFieldset = document.getElementById('activities');
+let totalCost = document.getElementById('activities-cost');
+let cost = 0;
 
-///***FORM VALIDATION FUNCTIONS (REGEX)***///
-//The following helper functions will validate each section of the form.
-let validateName = () => {
-  let nameReg = /\S+/;
-  return nameReg.test(nameField.value);
-};
+//Tuesday 9am-12pm
 
-let validateEmail = () => {
-  let emailReg = /^[^@]+@[^@.]+\.[a-z]+$/i;
-  return emailReg.test(emailField.value);
-};
+//This EV, and the code inside, will calculate the total cost of the tickets depending on the selection.
+activitySectionFieldset.addEventListener('change', (e) => {
+  target = e.target;
 
-let jobSelect = () => {
-  if (jobRoleSelection.value === 'Select Job Role') {
-    return false
+  if (target.checked) {
+    cost += parseInt(target.getAttribute('data-cost'));
+    totalCost.textContent = `Total: $${cost}`;
   } else {
-    return true;
-}};
+    cost -= parseInt(target.getAttribute('data-cost'));
+    totalCost.textContent = `Total: $${cost}`;
+  }
+})
 
-let validateActivitySelection = () => {
-  if (totalAmount === 0) {
+
+///payment Section///
+const paymentDropdown = document.getElementById('payment');
+const cardSection = document.getElementById('credit-card');
+const paypalSection = document.getElementById('paypal');
+const bitcoinSection = document.getElementById('bitcoin');
+
+//Function to show Credit Card payment option by default
+function defaultOption() {
+  let ccOption = document.querySelector('[value="credit-card"]');
+  ccOption.selected = true;
+  paypalSection.style.display = 'none';
+  bitcoinSection.style.display = 'none';
+}
+defaultOption();
+
+//Function to hide/show relevant payment sections depending on user input
+let paymentMethods = () => {
+  paymentDropdown.addEventListener('change', (e) => {
+    target = e.target.value;
+    cardSection.style.display = 'none';
+    paypalSection.style.display = 'none';
+    bitcoinSection.style.display = 'none'
+    console.log(target);
+    if (target === 'credit-card') {
+      cardSection.style.display = '';
+      paypalSection.style.display = 'none';
+      bitcoinSection.style.display = 'none'
+    } else if (target === 'paypal') {
+      paypalSection.style.display = '';
+    } else {
+      bitcoinSection.style.display = '';
+    }
+  });
+}
+
+paymentMethods();
+
+
+///validation Helpers///
+
+const validateSuccess = (element) => {
+  element.parentElement.classList.add('valid');
+  element.parentElement.classList.remove('not-valid');
+  element.parentElement.lastElementChild.classList.add('hint');
+}
+
+const validateFail = (element) => {
+  element.parentElement.classList.remove('valid');
+  element.parentElement.classList.add('not-valid');
+  element.parentElement.lastElementChild.classList.remove('hint');
+}
+
+const validateJobSuccess = (element) => {
+  element.parentElement.style.borderColor = ''
+  document.querySelector('[for="title"]').classList.add('valid');
+  document.querySelector('[for="title"]').classList.remove('not-valid');
+}
+
+const validateJobFail = (element) => {
+  element.parentElement.style.borderColor = 'red'
+  document.querySelector('[for="title"]').classList.remove('valid');
+  document.querySelector('[for="title"]').classList.add('not-valid');
+}
+
+const validateActivitiesSuccess = (element) => {
+  element.classList.add('valid');
+  element.classList.remove('not-valid');
+  element.firstElementChild.parentElement.lastElementChild.classList.add('hint')
+}
+
+const validateActivitiesFail = (element) => {
+  element.classList.add('not-valid');
+  element.classList.remove('valid');
+  element.firstElementChild.parentElement.lastElementChild.classList.remove('hint')
+}
+
+
+const nameValidator = () => {
+  const nameIsValid = /\S+/.test(nameField.value);
+  if (nameIsValid) {
+    validateSuccess(nameField);
+  } else {
+    validateFail(nameField);
+  }
+  return nameIsValid;
+};
+
+const emailValidator = () => {
+  const emailIsValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailField.value);
+  if (emailIsValid) {
+    validateSuccess(emailField);
+  } else {
+    validateFail(emailField);
+  }
+  return emailIsValid;
+}
+
+const cardValidator = () => {
+  const cardIsValid = /^(\d{13,16})$/g.test(cardNumField.value)
+  if (cardIsValid) {
+    validateSuccess(cardNumField);
+  } else {
+    validateFail(cardNumField);
+  }
+  return cardIsValid;
+}
+
+const zipValidator = ()=> {
+  const zipIsValid = /^(\d{5})$/g.test(zipCodeField.value)
+  if (zipIsValid){
+    validateSuccess(zipCodeField);
+  } else {
+    validateFail(zipCodeField);
+  }
+  return zipIsValid;
+};
+
+
+const cvvValidator = () => {
+  let cvvIsValid = /^(\d{3})$/g.test(cvvCodeField.value);
+  if (cvvIsValid) {
+    validateSuccess(cvvCodeField);
+  } else {
+    validateFail(cvvCodeField);
+  }
+  return cvvIsValid;
+};
+
+
+const jobSelectionValidator = () => {
+  if (title.value === 'Select Job Role') {
+    validateJobFail(title[0]);
+    console.log('failed job select')
     return false;
   } else {
+    validateJobSuccess(title[0]);
     return true;
-}};
+  }
+}
 
-let validateCardNum = () => {
-  const cardNumReg = /^(\d{13,16})$/g;
-  return cardNumReg.test(cardNumberField.value);
-};
 
-let validateZipCode = () => {
-  const zipCodeReg = /^(\d{5})$/g;
-  return zipCodeReg.test(zipCodeField.value);
-};
-
-let validateCVV = () => {
-  const cvvReg = /^(\d{3})$/g;
-  return cvvReg.test(cvvField.value);
-};
-
-///***FIELD SECTION STYLE ADD/REMOVE FUNCTION***///
-//This function will accept the functions above as a callback and add/remove validation styling as well as preventing default behaviour if invalid.
-let validateAllFunction = (functionName, element, event, message) => {
-  if (functionName() === false) {
-    element.parentElement.classList.add('not-valid');
-    element.parentElement.classList.remove('valid');
-    element.parentElement.lastElementChild.classList.remove('hint');
-    console.log(message)
-    event.preventDefault();
+const activityValidator = () => {
+  if (cost === 0) {
+    validateActivitiesFail(activitiesFieldset);
+    return false;
   } else {
-    element.parentElement.classList.add('valid');
-    element.parentElement.classList.remove('not-valid');
-    element.parentElement.lastElementChild.classList.add('hint');
+    validateActivitiesSuccess(activitiesFieldset);
+    return true;
+  }
+}
+
+
+//real-time validation feedback///
+nameField.addEventListener('keyup', nameValidator);
+emailField.addEventListener('keyup', emailValidator);
+cardNumField.addEventListener('keyup', cardValidator);
+zipCodeField.addEventListener('keyup', zipValidator);
+cvvCodeField.addEventListener('keyup', cvvValidator);
+title.addEventListener('change', jobSelectionValidator)
+activitySectionFieldset.addEventListener('change', activityValidator);
+
+//submit button validator///
+const form = document.querySelector('form');
+
+const clickValidator = (validatorFunc) => {
+  if (validatorFunc() === false) {
+    event.preventDefault();
   }
 };
 
-///***JOB SELECTION DROPDOWN STYLE ADD/REMOVE FUNCTION***///
-//I created this seperate function for the Job Role dropdown menu. This was not..
-//..required but I wanted to do it anyway.
-let validateJobSelection = (functionName, element, event, message) => {
-  if (functionName() === false) {
-    element.style.borderColor = 'red';
-    console.log('No Job Selected')
-    event.preventDefault();
-    jobSelection.lastElementChild.classList.remove('hint');
-  } else {
-    element.style.borderColor = '';
-    element.parentElement.classList.add('valid');
-    jobSelection.lastElementChild.classList.add('hint');
-  }
-};
+form.addEventListener('submit', (e) => {
+  clickValidator(nameValidator);
+  clickValidator(emailValidator);
+  clickValidator(activityValidator);
+  clickValidator(jobSelectionValidator);
 
-///***FORM EVENT HANDLER***///
-//This function will listen to submit events and run the validators against each field.
-form.addEventListener('submit', (event) => {
-  const target = event.target;
-  const creditCardSelection = document.getElementById('credit-card');
-
-  //This code will validate all fields up to the payment section.
-  validateAllFunction(validateName, nameField, event, 'invalid name');
-  validateAllFunction(validateEmail, emailField, event, 'invalid email');
-  validateAllFunction(validateActivitySelection, activitySelect.firstElementChild, event, 'No Activity Selected')
-  validateJobSelection(jobSelect, jobRoleSelection, event, 'No Job Role Selected')
-  // This IF statement will only run if the user selects Credit Card as their preferred payment method..
-  //..and will validate the card fields.
-  if (creditCardSelection.style.display === '') {
-    validateAllFunction(validateCardNum, cardNumberField, event, 'invalid card number')
-    validateAllFunction(validateZipCode, zipCodeField, event, 'invalid ZIP code')
-    validateAllFunction(validateCVV, cvvField, event, 'invalid CVV')
+  if (cardSection.style.display === '') {
+    clickValidator(cardValidator);
+    clickValidator(zipValidator);
+    clickValidator(cvvValidator);
   }
 });
 
 
-///***ACCESSIBILITY EVENT HANDLER***///
-//This code allows for tabbing over the checkbox section and will highlight/dehighlight each checkbox accordingly
-const checkboxes = document.querySelectorAll('input[type=checkbox]');
+///Assessibility///
 
-Array.from(checkboxes).forEach((checkbox) => {
-  let checkboxElement = checkbox;
+activitiesFieldset = document.getElementById('activities');
+activitiesBox = document.querySelectorAll('[type="checkbox"]');
 
-  checkboxElement.addEventListener('focus', (e) => {
-    let target = e.target;
-    let checkboxParent = target.parentNode;
-    checkboxParent.classList.add('focus');
+Array.from(activitiesBox).forEach((activity) => {
+  let activityCheck = activity;
+
+  activity.addEventListener('focus', (e) => {
+    target = e.target;
+    target.parentElement.className = 'focus';
   });
 
-  checkboxElement.addEventListener('blur', (e) => {
-    let target = e.target;
-    let checkboxParent = target.parentNode;
-    checkboxParent.classList.remove('focus');
-  });
-});
+  activity.addEventListener('blur', (e) => {
+    target = e.target;
+    target.parentElement.className = 'blur';
+  })
+})
